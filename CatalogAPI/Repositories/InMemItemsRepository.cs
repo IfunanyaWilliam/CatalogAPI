6755aa38f456bd076c08beee71e4a6ebffa22c84
IfunanyaWilliam <williamifunanya@gmail.com>
2022-08-30 17:06:07 +0100

    Add Item class and in memory class

---
+++ CatalogAPI/Repositories/InMemItemsRepository.cs
@@ -0,0 +1,18 @@
+﻿using CatalogAPI.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace CatalogAPI.Repositories
+{
+    public class InMemItemsRepository
+    {
+        private readonly List<Item> Items = new ()
+        {
+            new Item { Id = Guid.NewGuid(), Name = "", Price = 9, CreatedDate = DateTimeOffset.UtcNow },
+            new Item { Id = Guid.NewGuid(), Name = "", Price = 10, CreatedDate = DateTimeOffset.UtcNow },
+            new Item { Id = Guid.NewGuid(), Name = "", Price = 11, CreatedDate = DateTimeOffset.UtcNow },
+            new Item { Id = Guid.NewGuid(), Name = "", Price = 13, CreatedDate = DateTimeOffset.UtcNow },
+        };
+    }
+}
+ 
